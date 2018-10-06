e296344be6d373ab40be9acb093976522e95054f
Andrey Ilin <andrey.ilyin83@gmail.com>
2018-10-06 16:38:40 +0300

    tokens auth was added

--- index.js
+++ index.js
@@ -109,5 +109,6 @@ var unifiedServer = function (req, res) {
 // Define a request routing
 var router = {
     'ping': handlers.ping,
-    'users': handlers.users
+    'users': handlers.users,
+    'tokens': handlers.tokens
 };
